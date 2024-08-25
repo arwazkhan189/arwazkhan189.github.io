919998efe8d9452227f84125c0b399a19527dc7c
arwazkhan189 <arwazkhan189@gmail.com>
2024-08-25 13:01:30 +0530

    update

--- assets/main.js
+++ assets/main.js
@@ -35,7 +35,7 @@ $(".back-to-top").click(function () {
 */
 var typed = new Typed(".typejs", {
   strings: [
-    "MTech CSE Student | NIT Delhi " +
+    "MTech CSE'26 | NIT Delhi " +
     "<img src='./assets/images/NITD_logo.svg' width='30px'>",
     "Android App Developer " +
     "<img src='./assets/images/skills_image/android-developer.svg' width='30px'>",
